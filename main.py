db10d04a4ca7e448c476bb16879be49429d4f0ca
Patriot777 <zolinbest@gmail.com>
2021-02-19 15:32:25 +0200

    Update 2021

---
+++ main.py
@@ -0,0 +1,32 @@
+import eel
+import sqlite3
+
+#Підключення до бази данних
+#db = sqlite3.connect('db_task.db')
+#cursor = db.cursor()
+
+# Ініціалізація коду
+eel.init('web', allowed_extensions=['.js', '.html'])
+
+
+
+
+
+
+
+def returnet_data():
+    db = sqlite3.connect('db_task.db')
+    cursor = db.cursor()
+    cursor.execute("SELECT title, text FROM tasks")
+    title = cursor.fetchall()
+    eel.public_task(title)
+
+@eel.expose
+def add():
+    
+    #cursor.execute(f"""INSERT INTO tasks (text) VALUES ('{ff}')""")
+    #db.commit()
+    returnet_data()
+
+eel.start('index.html', size=(850, 500), jinja_templates="templates")
+
---
+++ web/eel.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('button');
+var data = document.addEventListener('num1')
+button.addEventListener('click', ()=> {
+    alert(data)
+});
+
+
