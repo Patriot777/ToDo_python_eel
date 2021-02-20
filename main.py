0989bb66aee64673d5c2f6d6121cd84d0782acf5
Patriot777 <zolinbest@gmail.com>
2021-02-20 18:41:28 +0200

    Create table

--- main.py
+++ main.py
@@ -8,25 +8,23 @@ import sqlite3
 # Ініціалізація коду
 eel.init('web', allowed_extensions=['.js', '.html'])
 
-
-
-print("hello GIT")
-
-
-
 def returnet_data():
     db = sqlite3.connect('db_task.db')
     cursor = db.cursor()
     cursor.execute("SELECT title, text FROM tasks")
     title = cursor.fetchall()
     eel.public_task(title)
+    db.close()
 
+#Функція яка визивається з JS та повертає у html дані з бази.
 @eel.expose
 def add():
-    
-    #cursor.execute(f"""INSERT INTO tasks (text) VALUES ('{ff}')""")
-    #db.commit()
     returnet_data()
+
+@eel.expose
+def test(title, text):
+    print(title)
+    print(text)
 
 eel.start('index.html', size=(1366, 768), jinja_templates="templates")
 
